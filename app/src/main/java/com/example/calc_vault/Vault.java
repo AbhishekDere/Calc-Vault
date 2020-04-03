43e6d4a7002ffb1241e7039e2c6a2b2a52dabb69
Abhishek <44259614+xanny69@users.noreply.github.com>
2020-04-03 14:28:46 +0530

    commit 2

--- app/src/main/java/com/example/calc_vault/Vault.java
+++ app/src/main/java/com/example/calc_vault/Vault.java
@@ -2,25 +2,36 @@ package com.example.calc_vault;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class Vault extends AppCompatActivity {
-    ImageView button_photo = findViewById(R.id.imageView);
-    ImageView button_videos = findViewById(R.id.imageView2);
-    ImageView button_files = findViewById(R.id.imageView3);
-    ImageView button_audio = findViewById(R.id.imageView4);
-    ImageView button_settings = findViewById(R.id.imageView5);
-    ImageView button_encryption = findViewById(R.id.imageView6);
+
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vault);
-    }
-    protected void onClicked(){
-
+        //Setting the Buttons
+        ImageView button_photo = findViewById(R.id.imageView);
+        ImageView button_videos = findViewById(R.id.imageView2);
+        ImageView button_files = findViewById(R.id.imageView3);
+        ImageView button_audio = findViewById(R.id.imageView4);
+        ImageView button_settings = findViewById(R.id.imageView5);
+        ImageView button_encryption = findViewById(R.id.imageView6);
+        //Setting OnClickListeners to Each Buttons
+        button_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Photos.class);
+                startActivity(intent);
+            }
+        });
     }
 }
